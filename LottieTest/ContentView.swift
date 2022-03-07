11767b142d8a3bb299701ef9bbc410639752cdad
Faiz Mokhtar <mfmokhtar@gmail.com>
2022-03-07 13:54:57 +0800

    Initial Commit

---
+++ LottieTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LottieTest
+//
+//  Created by AD0502-ADE-MB-1 on 07/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LottieTest/LottieTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieTestApp.swift
+//  LottieTest
+//
+//  Created by AD0502-ADE-MB-1 on 07/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct LottieTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
